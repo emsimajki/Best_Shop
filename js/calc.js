82999b9567fa871d5b116885db44899473d213f1
emsimajki <mateuszstepien25@gmail.com>
2020-09-15 15:53:09 +0200

    add calc JS

---
+++ js/calc.js
@@ -0,0 +1,13 @@
+function Calculator(form, summary) {
+    this.prices = {
+        products: 0.5,
+        orders: 0.25,
+        package: {
+            basic: 0,
+            professional: 25,
+            premium: 60
+        },
+        accounting: 35,
+        terminal: 5
+    };
+}
